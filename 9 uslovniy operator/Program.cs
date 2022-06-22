1c6c19d3d3415c37d49557e71fc24ddf9b3dc32b
Георгий Князев <saint_georgy@yahoo.com>
2022-06-22 13:25:53 +0300

    Добавьте файлы проекта.

---
+++ 9 uslovniy operator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _9_uslovniy_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0,28);
+            int minimumRangeValue = 100;
+            int maximumRangeValue = 1000;
+            Console.WriteLine(randomNumber);
+
+            for (int i = randomNumber; i < maximumRangeValue; i+= randomNumber)
+            {
+                if (i <= minimumRangeValue)
+                {
+                    continue;
+                }
+                else if (minimumRangeValue <= i || i < maximumRangeValue)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
